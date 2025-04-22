6f21823bb68de386d7a23d2c7a075132b37dbc65
LAPTOP-MDR1JLK9\User <trollfacetnt2@gmail.com>
2025-04-22 15:29:27 -0400

    no message

---
+++ Assets/textdisplayer.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class textdisplayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject toptxt;
+    public GameObject bottomtxt;
+    public float toptimer;
+    public bool toptimerenabled;
+    public float bottomtimer;
+    public bool bottomtimerenabled;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(toptimerenabled)
+        {
+        if(toptimer > 0)
+        {
+            toptimer -= Time.deltaTime;
+
+        }
+        else
+        {
+            toptimerenabled = false;
+            toptxt.getComponet<TMP_Text>().text = "";
+        }
+        }
+        if(bottomtimertimerenabled)
+        {
+        if(bottomtimer > 0)
+        {
+            bottomtimer -= Time.deltaTime;
+
+        }
+        else
+        {
+            bottomtimerenabled = false;
+            bottomtxt.getComponet<TMP_Text>().text = "";
+        }
+        }
+    }
+    public void settoptext(string text, float time)
+    {
+        toptxt.transform.getComponet<TMP_Text>().text = text;
+        if(time > 0)
+        {
+            toptimerenabled = true;
+            toptimer = time;
+        }
+        else
+        {
+            toptimerenabled = false;
+        }
+    }
+    public void setbottomtext(string text, float time)
+    {
+        bottomtxt.transform.getComponent<TMP_Text>().text = text;
+        if(time > 0)
+        {
+            bottomtimerenabled = true;
+            bottomtimer = time;
+        }
+        else
+        {
+            bottomtimerenabled = false;
+        }
+    }
+}
